c28d537b7df2ee30d1c843bc30674776dbf1e293
Pawel Pikula <ppikula@gmail.com>
2017-11-23 21:21:41 +0100

    initial forumla

---
+++ Formula/perun.rb
@@ -0,0 +1,26 @@
+class Perun < Formula
+  desc "Swiss army knife for AWS CloudFormation templates"
+  homepage "https://github.com/Appliscale/perun"
+  url "https://github.com/Appliscale/perun/archive/1.0.0.tar.gz"
+  sha256 "1301ce5e60bf0958d297b469c21a727fb18f5de8be340755889d857bae4d7295"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV.prepend_create_path "PATH", buildpath/"bin"
+
+    dir = buildpath/"src/github.com/Appliscale/perun"
+    dir.install buildpath.children - [buildpath/".brew_home"]
+
+    cd dir do
+      system "make", "all"
+    end
+
+    bin.install "bin/perun"
+  end
+
+  test do
+    system "#{bin}/perun", "--help"
+  end
+end
