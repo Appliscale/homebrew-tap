ef9ab466e8b2c8690265684578a7f07144debad7
Sylwia <30501500+SylwiaGargula@users.noreply.github.com>
2018-05-17 10:52:17 +0200

    Update perun.rb

--- Formula/perun.rb
+++ Formula/perun.rb
@@ -1,7 +1,7 @@
 class Perun < Formula
   desc "Swiss army knife for AWS CloudFormation templates"
   homepage "https://github.com/Appliscale/perun"
-  url "https://github.com/Appliscale/perun/archive/1.0.0.tar.gz"
+  url "https://github.com/Appliscale/perun/archive/1.1.0.tar.gz"
   sha256 "1301ce5e60bf0958d297b469c21a727fb18f5de8be340755889d857bae4d7295"
 
   depends_on "go" => :build
