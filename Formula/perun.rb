80ae81fa0758962bf63f6aa54977480368824dc3
Maksymilian Wojczuk <max.wojczuk@npspace.pl>
2018-09-10 11:07:40 +0200

    Update perun.rb
    Changed Release version

--- Formula/perun.rb
+++ Formula/perun.rb
@@ -1,7 +1,7 @@
 class Perun < Formula
   desc "Swiss army knife for AWS CloudFormation templates"
   homepage "https://github.com/Appliscale/perun"
-  url "https://github.com/Appliscale/perun/archive/1.1.0.tar.gz"
+  url "https://github.com/Appliscale/perun/archive/1.3.0.tar.gz"
   sha256 "cdf194a27337326d25530c2fed2b48bf6c7a6b41be980f2f2a6d614f7f9b2142"
 
   depends_on "go" => :build
