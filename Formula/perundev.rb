cb872f85608f39f349a6dc9400ac41e0968b9c76
sgargula <sylwia.gargula@wp.pl>
2018-09-25 13:53:11 +0200

    sha256sum

--- Formula/perundev.rb
+++ Formula/perundev.rb
@@ -2,7 +2,7 @@ class Perundev < Formula
   desc "Swiss army knife for AWS CloudFormation templates"
   homepage "https://github.com/Appliscale/perun"
   url "https://github.com/Appliscale/perun/archive/1.3.1.tar.gz"
-  sha256 "c6ce0c9f748a1d86b1ba8987937f5976e8632b1ea87a078d55b5316b01cf1258"
+  sha256 "f56b5993161293dd3f72f597a24700d0a2841909b8650231cbfd1edbb3217da1"
 
   depends_on "go" => :build
 
